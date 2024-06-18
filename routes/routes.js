dc1698ed8237c940d4548f2224d1e2016d1a6169
gerismumo <geraldmumo6@gmail.com>
2024-06-18 06:48:43 +0300

    changes on routes

--- routes/routes.js
+++ routes/routes.js
@@ -3,7 +3,7 @@ const controller = require('../controllers/controller');
 const callBack = require('../controllers/callback');
 const router  = express.Router();
 
-router.post('/text-form', controller.payment);
+router.post('/express-payment', controller.payment);
 router.post('/callback', callBack)
 
 module.exports = router;
